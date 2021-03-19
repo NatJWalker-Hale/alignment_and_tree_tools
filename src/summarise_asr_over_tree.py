0f6aff3632d2fd1b6d2707dabf1274919d7eb14d
NatJWalker-Hale <nathanael.walker.hale@gmail.com>
2021-03-19 17:13:00 +0000

    updates

--- src/summarise_asr_over_tree.py
+++ src/summarise_asr_over_tree.py
@@ -7,7 +7,31 @@ from parse_fasta import parse_fasta
 from treenode import Node
 
 
+def count_diffs(seq1, seq2):
+    """iterate over two aligned sequences and return differences
+    as state1posstate2, e.g. A235V"""
+    if len(seq1) != len(seq2):
+        sys.stderr.write(seq1 + "not equal in length to" + seq2)
+    rawDiffs = [(j[0], i+1, j[1]) for i, j in
+                enumerate(zip(seq1, seq2)) if j[0] != j[1]]
+    diffs = [j[0] + str(j[1]) + j[2] for j in rawDiffs]
+    return diffs
 
+
+def get_anc_desc(node):
+    """iterate over all nodes in a node-labelled tree and create
+    a dictionary of ancestor descendant relationships, where each
+    entry is one branch"""
+    brDict = {}
+    for n in node.iternodes(order="preorder"):
+        for c in n.children:
+            brDict[(n.label, c.label)] = []
+    return brDict
+
+
+def add_subs(brDict, seqDict):
+    for k in brDict.keys():
+        brDict[k] += count_diffs(seqDict[k[0]], seqDict[k[1]])
 
 
 if __name__ == "__main__":
@@ -22,9 +46,22 @@ if __name__ == "__main__":
                         sequences")
     args = parser.parse_args()
 
-    with open(tFile, "r") as t:
+    with open(args.tree, "r") as t:
         for s in t:
             s = s.strip()
             nwkString = s
             
     curroot = tree_reader.read_tree_string(nwkString)
+    branches = get_anc_desc(curroot)
+    
+    seqs = dict([x for x in parse_fasta(args.sequences)])
+    print(seqs)
+
+    add_subs(branches, seqs)
+
+    print("anc\tdesc\tsubs")
+    for k, v in branches.items():
+        print(k[0] + "\t" + k[1] + "\t" + ",".join(v))
+    
+
+    

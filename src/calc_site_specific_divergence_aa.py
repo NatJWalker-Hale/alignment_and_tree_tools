2572ca3d33ca98d6177b80303640219fc81d0891
natjwalkerhale <nathanael.walker.hale@gmail.com>
2021-09-01 13:49:13 +0100

    updates

--- src/calc_site_specific_divergence_aa.py
+++ src/calc_site_specific_divergence_aa.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python3
 
 import sys
-import os
 import argparse
 from collections import Counter
+from scipy.spatial import distance
 from parse_fasta import parse_fasta
 
 
@@ -45,16 +45,41 @@ def calc_euclidean(colPropDict1, colPropDict2):
     return distDict
 
 
+def calc_kl(colProp1, colProp2):
+    div = [x * math.log((x / y)) for x, y in zip(colProp1, colProp2) if y != 0 and x != 0]
+    kl = sum(div)
+    return kl
+
+
+def calc_jsd(colPropDict1, colPropDict2):
+    distDict = {}
+    for k, v in colPropDict1.items():
+        m = [(x + y) / 2 for x, y in zip(v, colPropDict2[k])]
+        jsd = (calc_kl(v, m) / 2) + (calc_kl(colPropDict2[k], m) / 2)
+        distDict[k] = jsd
+    return distDict
+
+
+def calc_jsd_scipy(colPropDict1, colPropDict2):
+    distDict = {}
+    for k, v in colPropDict1.items():
+        distDict[k] = distance.jensenshannon(v, colPropDict2[k]) ** 2
+    return distDict
+
+
 if __name__ == "__main__":
     if len(sys.argv[1:]) == 0:
         sys.argv.append("-h")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--sequence", help="Alignment in FASTA format")
-    parser.add_argument("-f1", "--flag1", help="Flag to recognise sequences \
+    parser.add_argument("sequence", help="Alignment in FASTA format")
+    parser.add_argument("flag1", help="Flag to recognise sequences \
                         in group 1. Must be first substring of name.")
-    parser.add_argument("-f2", "--flag2", help="Flag to recognise sequences \
+    parser.add_argument("flag2", help="Flag to recognise sequences \
                         in group 2. Must be first substring of name.")
+    parser.add_argument("-d", "--distance", help="distance \
+                        metric to use [euc (default)/jsd]",
+                        default="euc")
     args = parser.parse_args()
 
     seqs1 = dict([x for x in parse_fasta(args.sequence)
@@ -63,11 +88,17 @@ if __name__ == "__main__":
                  if x[0].startswith(args.flag2)])
     cols1 = get_columns(seqs1)
     cols2 = get_columns(seqs2)
-    colProp1 = calc_col_prop(cols1)
+    colProps1 = calc_col_prop(cols1)
     # print(colProp1)
-    colProp2 = calc_col_prop(cols2)
+    colProps2 = calc_col_prop(cols2)
     # print(colProp2)
-    dist = calc_euclidean(colProp1, colProp2)
+    if args.distance == "euc":
+        dist = calc_euclidean(colProps1, colProps2)
+    elif args.distance == "jsd":
+        dist = calc_jsd_scipy(colProps1, colProps2)
+    else:
+        print("distance argument not recognised")
+        sys.exit()
     print("pos,dist")
     tmp = [(a+1, b) for a, b in zip(sorted(dist, key=dist.get, reverse=True), sorted(dist.values(), reverse=True))]
     for i in tmp:

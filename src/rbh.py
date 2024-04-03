c6c9b5b8d239631f70e4584b4916ade0eccc3d0b
Nathanael Walker-Hale <nathanael.walker.hale@gmail.com>
2024-04-03 15:57:51 +0100

    minor updates

--- src/rbh.py
+++ src/rbh.py
@@ -8,9 +8,14 @@ Script to call Reciprocal Best Hits from 1-2 2-1 blast results in outfmt6
 
 import sys
 import argparse
+from typing import Iterator
 
 
-def get_rbh(in1: str, in2: str) -> list:
+def get_rbh(in1: str, in2: str) -> list[tuple[str, str]]:
+    """
+    returns a list of reciprocal best hits between query-reference blast in in1 and
+    reference-query blast in in2
+    """
     res1 = {}
     with open(in1, "r", encoding="utf-8") as inf:
         for line in inf:
@@ -36,15 +41,38 @@ def get_rbh(in1: str, in2: str) -> list:
     return rbh
 
 
+def get_bh(in1: str) -> Iterator[tuple]:
+    """
+    yields a tuple of top hit per query in reference-query blast results in in1
+    """
+    res1 = {}
+    with open(in1, "r", encoding="utf-8") as inf:
+        for line in inf:
+            line = line.strip().split()
+            if line[0] in res1:
+                continue
+            res1[line[0]] = line[1:]
+            yield line[0], line[1]
+
+
 if __name__ == "__main__":
     if len(sys.argv[1:]) == 0:
         sys.argv.append("-h")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("blast_hits1")
-    parser.add_argument("blast_hits2")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-rbh", "--reciprocal_best_hits", help="query-reference and \
+                        reference-query blast results in outfmt6", type=str, nargs=2)
+    group.add_argument("-bh", "--best_hits", help="query-reference blast results in outfmt6",
+                       type=str)
     args = parser.parse_args()
 
-    rbhs = get_rbh(args.blast_hits1, args.blast_hits2)
-    for i in rbhs:
-        print(f"{i[0]}\t{i[1]}")
+    if args.reciprocal_best_hits is not None:
+        f1, f2 = args.reciprocal_best_hits
+        rbhs = get_rbh(f1, f2)
+        for i in rbhs:
+            print(f"{i[0]}\t{i[1]}")
+    
+    if args.best_hits is not None:
+        for i in get_bh(args.best_hits):
+            print(f"{i[0]}\t{i[1]}")

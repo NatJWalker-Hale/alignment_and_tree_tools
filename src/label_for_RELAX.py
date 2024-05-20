b670d9de2f2c7e7461d4a1e5c51e3595b7f2bf67
NatJWalker-Hale <nathanael.walker.hale@gmail.com>
2024-05-20 12:44:08 +0100

    adds further labelling modes for RELAX and paml

--- src/label_for_RELAX.py
+++ src/label_for_RELAX.py
@@ -16,20 +16,34 @@ if __name__ == "__main__":
                                      foreground for RELAX")
     parser.add_argument("tree", help="tree topology to label. Can have branch lengths. Internal \
                         nodes will be overwritten if present")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-r", "--relax", help="hyphy-style labels for RELAX", action="store_const",
+                       dest="type", const="relax", default="relax")
+    group.add_argument("-p", "--paml", help="codeml-style labels for branch models", dest="type",
+                       action="store_const", const="paml")
+    parser.add_argument("-b", "--brlen", help="write branch lengths", action="store_true")
     args = parser.parse_args(sys.argv[1:] or ["--help"])
 
     curroot = newick3.parse_from_file(args.tree)
 
     for n in curroot.iternodes(order=0):
+        if not args.brlen:
+            n.length = None
         if n.parent is None:
             continue
         if n.label == "PH01S":
-            n.label += "{Test}"
-        else:
-            if n.istip:
-                n.label += "{Reference}"
+            if args.type == "relax":
+                n.label += "{Test}"
             else:
-                n.label = "{Reference}"
+                n.label += "#1"
+        else:
+            if args.type == "relax":
+                if n.istip:
+                    n.label += "{Reference}"
+                else:
+                    n.label = "{Reference}"
+            else:
+                if not n.istip:
+                    n.label = None
 
     print(newick3.to_string(curroot) + ";")
-    

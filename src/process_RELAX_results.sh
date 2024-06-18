84bed48da4b29fa7651a7d7117d6fd5d9d02376c
NatJWalker-Hale <nathanael.walker.hale@gmail.com>
2024-06-18 14:15:41 +0100

    commits changes to an earlier script as a new version for prettier tables, rolls back to original version

--- src/process_RELAX_results.sh
+++ src/process_RELAX_results.sh
@@ -9,7 +9,8 @@ Help()
 {
     echo "Run this script in a working dir with multiple RELAX jsons. Requires jq."
     echo "writes RELAX output from alternative or partitioned descriptive model to tsv"
-    echo "Usage: bash process_RELAX_output.sh"
+    echo "Usage: bash process_RELAX_output.sh [MODEL]"
+    echo "  MODEL: Ralt (RELAX alternative) or PD (partitioned descriptive)"
 }
 
 while :; do
@@ -21,64 +22,58 @@ while :; do
     break
 done
 
-if compgen -G "$(pwd)/*.RELAX.json" > /dev/null; then
-    echo "Found RELAX results in $(pwd)" 1>&2
-else
-    echo "Did not find RELAX results in $(pwd)" 1>&2
-    exit
-fi
-
-
 echo $(ls *.RELAX.json) 1>&2
- 
+
 echo "Processing above files" 1>&2
 
-echo -e "gene\tmodel\tlnL\taicc\tlrt\tpval\tk\tpartition\tw0\tw1\tw2\tp0\tp1\tp2"
-for i in *.RELAX.json
-do
-gene=$(echo $i | cut -f1 -d".")
-lnLalt=$(cat $i | jq '.["fits"]["RELAX alternative"]["Log Likelihood"]')
-lnLnull=$(cat $i | jq '.["fits"]["RELAX null"]["Log Likelihood"]')
-lnLpd=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Log Likelihood"]')
-lrt=$(cat $i | jq '.["test results"]["LRT"]')
-pval=$(cat $i | jq '.["test results"]["p-value"]')
-k=$(cat $i | jq '.["test results"]["relaxation or intensification parameter"]')
-aiccnull=$(cat $i | jq '.["fits"]["RELAX null"]["AIC-c"]')
-aiccalt=$(cat $i | jq '.["fits"]["RELAX alternative"]["AIC-c"]')
-aiccpd=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["AIC-c"]')
-nw0=$(cat $i | jq '.["fits"]["RELAX null"]["Rate Distributions"]["Reference"]["0"]["omega"]')
-nw1=$(cat $i | jq '.["fits"]["RELAX null"]["Rate Distributions"]["Reference"]["1"]["omega"]')
-nw2=$(cat $i | jq '.["fits"]["RELAX null"]["Rate Distributions"]["Reference"]["2"]["omega"]')
-np0=$(cat $i | jq '.["fits"]["RELAX null"]["Rate Distributions"]["Reference"]["0"]["proportion"]')
-np1=$(cat $i | jq '.["fits"]["RELAX null"]["Rate Distributions"]["Reference"]["1"]["proportion"]')
-np2=$(cat $i | jq '.["fits"]["RELAX null"]["Rate Distributions"]["Reference"]["2"]["proportion"]')
-rw0=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["0"]["omega"]')
-rw1=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["1"]["omega"]')
-rw2=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["2"]["omega"]')
-tw0=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["0"]["omega"]')
-tw1=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["1"]["omega"]')
-tw2=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["2"]["omega"]')
-rp0=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["0"]["proportion"]')
-rp1=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["1"]["proportion"]')
-rp2=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["2"]["proportion"]')
-tp0=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["0"]["proportion"]')
-tp1=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["1"]["proportion"]')
-tp2=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["2"]["proportion"]')
-pdrw0=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["0"]["omega"]')
-pdrw1=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["1"]["omega"]')
-pdrw2=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["2"]["omega"]')
-pdtw0=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["0"]["omega"]')
-pdtw1=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["1"]["omega"]')
-pdtw2=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["2"]["omega"]')
-pdrp0=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["0"]["proportion"]')
-pdrp1=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["1"]["proportion"]')
-pdrp2=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["2"]["proportion"]')
-pdtp0=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["0"]["proportion"]')
-pdtp1=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["1"]["proportion"]')
-pdtp2=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["2"]["proportion"]')
-echo -e "$gene\tnull\t$lnLnull\t$aiccnull\t\t\t1\tReference\t$nw0\t$nw1\t$nw2\t$np0\t$np1\t$np2"
-echo -e "$gene\talt\t$lnLalt\t$aiccalt\t$lrt\t$pval\t$k\tReference\t$rw0\t$rw1\t$rw2\t$rp0\t$rp1\t$rp2"
-echo -e "$gene\talt\t\t\t\t\t\tTest\t$tw0\t$tw1\t$tw2\t$tp0\t$tp1\t$tp2"
-echo -e "$gene\tPD\t$lnLpd\t$aiccpd\t\t\t\tReference\t$pdrw0\t$pdrw1\t$pdrw2\t$pdrp0\t$pdrp1\t$pdrp2"
-echo -e "$gene\tPD\t\t\t\t\t\tTest\t$pdtw0\t$pdtw1\t$pdtw2\t$pdtp0\t$pdtp1\t$pdtp2"
-done
+if [[ "$1" == "Ralt" ]]
+then
+    echo -e "gene\tpartition\tlnLnull\tlnLalt\tlrt\tpval\tk\taicc\tw0\tw1\tw2\tp0\tp1\tp2"
+    for i in *.RELAX.json
+    do
+    gene=$(echo $i | cut -f1 -d".")
+    lnLalt=$(cat $i | jq '.["fits"]["RELAX alternative"]["Log Likelihood"]')
+    lnLnull=$(cat $i | jq '.["fits"]["RELAX null"]["Log Likelihood"]')
+    lrt=$(cat $i | jq '.["test results"]["LRT"]')
+    pval=$(cat $i | jq '.["test results"]["p-value"]')
+    k=$(cat $i | jq '.["test results"]["relaxation or intensification parameter"]')
+    aicc=$(cat $i | jq '.["fits"]["RELAX alternative"]["AIC-c"]')
+    rw0=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["0"]["omega"]')
+    rw1=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["1"]["omega"]')
+    rw2=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["2"]["omega"]')
+    tw0=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["0"]["omega"]')
+    tw1=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["1"]["omega"]')
+    tw2=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["2"]["omega"]')
+    rp0=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["0"]["proportion"]')
+    rp1=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["1"]["proportion"]')
+    rp2=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Reference"]["2"]["proportion"]')
+    tp0=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["0"]["proportion"]')
+    tp1=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["1"]["proportion"]')
+    tp2=$(cat $i | jq '.["fits"]["RELAX alternative"]["Rate Distributions"]["Test"]["2"]["proportion"]')
+    echo -e "$gene\treference\t$lnLnull\t$lnLalt\t$lrt\t$pval\t$k\t$aicc\t$rw0\t$rw1\t$rw2\t$rp0\t$rp1\t$rp2"
+    echo -e "$gene\ttest\t$lnLnull\t$lnLalt\t$lrt\t$pval\t$k\t$aicc\t$tw0\t$tw1\t$tw2\t$tp0\t$tp1\t$tp2"
+    done
+elif [[ "$1" == "PD" ]]
+then
+    echo -e "gene\tpartition\tlnL\taicc\tw0\tw1\tw2\tp0\tp1\tp2"
+    for i in *.RELAX.json
+    do
+    gene=$(echo $i | cut -f1 -d".")
+    lnL=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Log Likelihood"]')
+    aicc=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["AIC-c"]')
+    rw0=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["0"]["omega"]')
+    rw1=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["1"]["omega"]')
+    rw2=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["2"]["omega"]')
+    tw0=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["0"]["omega"]')
+    tw1=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["1"]["omega"]')
+    tw2=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["2"]["omega"]')
+    rp0=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["0"]["proportion"]')
+    rp1=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["1"]["proportion"]')
+    rp2=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Reference"]["2"]["proportion"]')
+    tp0=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["0"]["proportion"]')
+    tp1=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["1"]["proportion"]')
+    tp2=$(cat $i | jq '.["fits"]["RELAX partitioned descriptive"]["Rate Distributions"]["Test"]["2"]["proportion"]')
+    echo -e "$gene\treference\t$lnL\t$aicc\t$rw0\t$rw1\t$rw2\t$rp0\t$rp1\t$rp2"
+    echo -e "$gene\ttest\t$lnL\t$aicc\t$tw0\t$tw1\t$tw2\t$tp0\t$tp1\t$tp2"
+    done
+fi

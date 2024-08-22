79a2ff01cbfa6078c45ef0cf5fde6a5f741ff311
NatJWalker-Hale <nathanael.walker.hale@gmail.com>
2024-08-22 18:21:55 +0100

    adds new script to convert FASTA to phylip

---
+++ src/fasta2phylip.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+"""
+converts FASTA-formatted alignments to non-interleaved phylip
+"""
+
+
+import sys
+import argparse
+# from Bio import SeqIO
+import sequence as sq
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="converts FASTA-formatted MSAs to PHYLIP format")
+    parser.add_argument("aln", help="FASTA-formatted input alignment")
+    args = parser.parse_args(sys.argv[1:] or ["--help"])
+
+    seqs = dict(sq.parse_fasta(args.aln))
+    print(sq.get_phylip_str(seqs))
